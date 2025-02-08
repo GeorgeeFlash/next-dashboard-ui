8bacb2aba20426bea746cd03ed9861b8f83ac988
georgeeflash <georgeeflash@gmail.com>
2025-02-08 04:38:10 +0100

    Attendance Chart data fetched. No errors.

--- src/components/AttendanceChartContainer.tsx
+++ src/components/AttendanceChartContainer.tsx
@@ -12,7 +12,7 @@ const AttendanceChartContainer = async () => {
 
   lastMonday.setDate(today.getDate() - daysSinceMonday);
 
-  const data = await prisma.attendance.findMany({
+  const resData = await prisma.attendance.findMany({
     where: {
       date: {
         gte: lastMonday,
@@ -24,14 +24,46 @@ const AttendanceChartContainer = async () => {
     },
   });
 
-  console.log(data);
+  // console.log(data);
+
+  const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri"];
+
+  const attendanceMap: { [key: string]: { present: number; absent: number } } =
+    {
+      Mon: { present: 0, absent: 0 },
+      Tue: { present: 0, absent: 0 },
+      Wed: { present: 0, absent: 0 },
+      Thu: { present: 0, absent: 0 },
+      Fri: { present: 0, absent: 0 },
+    };
+
+  resData.forEach((item) => {
+    const itemDate = new Date(item.date);
+
+    if (dayOfWeek >= 1 && dayOfWeek <= 5) {
+      const dayName = daysOfWeek[dayOfWeek - 1];
+
+      if (item.present) {
+        attendanceMap[dayName].present += 1;
+      } else {
+        attendanceMap[dayName].absent += 1;
+      }
+    }
+  });
+
+  const data = daysOfWeek.map((day) => ({
+    name: day,
+    present: attendanceMap[day].present,
+    absent: attendanceMap[day].absent,
+  }));
+
   return (
     <div className="bg-white rounded-lg p-4 h-full">
       <div className="flex justify-between items-center">
         <h1 className=" text-lg font-semibold">Attendance</h1>
         <Image src={"/moreDark.png"} alt="" width={20} height={20} />
       </div>
-      <AttendanceChart />
+      <AttendanceChart data={data} />
     </div>
   );
 };
