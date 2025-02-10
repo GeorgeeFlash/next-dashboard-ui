eaae61515993c055aa5a45a327408a1273832d00
georgeeflash <georgeeflash@gmail.com>
2025-02-10 12:54:29 +0100

    auth problem fixed. phew!

---
+++ components/forms/LessonForm.tsx
@@ -0,0 +1,154 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import InputField from "../InputField";
+import { ClassSchema, classSchema } from "../../lib/formValidationSchemas";
+import { createClass, updateClass } from "../../lib/actions";
+import { useFormState } from "react-dom";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
+
+// TODO: Finish this later
+const LessonForm = ({
+  setOpen,
+  type,
+  data,
+  relatedData,
+}: {
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  type: "create" | "update";
+  data?: any;
+  relatedData?: any;
+}) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClassSchema>({ resolver: zodResolver(classSchema) });
+
+  // After React 19 It'll be UseActionState
+  const [state, formAction] = useFormState(
+    type === "create" ? createClass : updateClass,
+    {
+      success: false,
+      error: false,
+    }
+  );
+
+  const onSubmit = handleSubmit((data) => {
+    formAction(data);
+  });
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (state.success) {
+      toast(`Class has been ${type === "create" ? "created." : "updated."}`);
+      setOpen(false);
+      router.refresh();
+    }
+  }, [state]);
+
+  const { teachers, grades } = relatedData;
+
+  return (
+    <form className="flex flex-col gap-8" onSubmit={onSubmit}>
+      <h1 className="text-xl font-semibold">
+        {type === "create" ? "Create a new Class" : "Update Class"}
+      </h1>
+      <div className="flex justify-between flex-wrap gap-4">
+        <InputField
+          label="Class name"
+          name="name"
+          defaultValue={data?.name}
+          register={register}
+          error={errors?.name}
+        />
+        <InputField
+          label="Capacity"
+          name="capacity"
+          defaultValue={data?.capacity}
+          register={register}
+          error={errors?.capacity}
+        />
+        {data && (
+          <InputField
+            label="Id"
+            name="id"
+            defaultValue={data?.id}
+            register={register}
+            error={errors?.id}
+            hidden
+          />
+        )}
+        <div className="flex flex-col gap-2 w-full md:w-1/4">
+          <label className="text-xs text-gray-500" htmlFor="">
+            Supervisor
+          </label>
+          <select
+            className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full"
+            {...register("supervisorId", { required: true })}
+            defaultValue={teachers.map(
+              (teacher: { id: string; name: string; surname: string }) =>
+                teacher.id
+            )}
+          >
+            {teachers.map(
+              (teacher: { id: string; name: string; surname: string }) => (
+                <option
+                  key={teacher.id}
+                  value={teacher.id}
+                  selected={data && teacher.id === data.supervisorId}
+                >
+                  {teacher.name + " " + teacher.surname}
+                </option>
+              )
+            )}
+          </select>
+          {errors?.supervisorId?.message && (
+            <p className="text-xs text-red-400">
+              {errors?.supervisorId.message.toString()}
+            </p>
+          )}
+        </div>
+        <div className="flex flex-col gap-2 w-full md:w-1/4">
+          <label className="text-xs text-gray-500" htmlFor="">
+            Grade
+          </label>
+          <select
+            className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full"
+            {...register("gradeId", { required: true })}
+            defaultValue={grades.map(
+              (grade: { id: number; level: number }) => grade.id
+            )}
+          >
+            {grades.map((grade: { id: string; level: number }) => (
+              <option
+                key={grade.id}
+                value={grade.id}
+                selected={data && grade.id === data.gradeId}
+              >
+                {grade.level}
+              </option>
+            ))}
+          </select>
+          {errors?.gradeId?.message && (
+            <p className="text-xs text-red-400">
+              {errors?.gradeId.message.toString()}
+            </p>
+          )}
+        </div>
+      </div>
+      {state.error && (
+        <span className="text-red-500">Something went wrong!</span>
+      )}
+      <button className="bg-blue-400 text-white p-2 rounded-md" type="submit">
+        {type === "create" ? "Create" : "Update"}
+      </button>
+    </form>
+  );
+};
+
+export default LessonForm;
---
+++ lib/actions.ts
@@ -0,0 +1,230 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import {clerkClient} from '@clerk/express';
+
+import {
+  ClassSchema,
+  SubjectSchema,
+  TeacherSchema,
+} from "./formValidationSchemas";
+import prisma from "./prisma";
+
+type CurrentState = { success: boolean; error: boolean };
+
+// Subject
+export const createSubject = async (
+  currentState: CurrentState,
+  data: SubjectSchema
+) => {
+  try {
+    await prisma.subject.create({
+      data: {
+        name: data.name,
+        teachers: {
+          connect: data.teachers.map((teacherId) => ({ id: teacherId })),
+        },
+      },
+    });
+
+    // revalidatePath("/list/subjects");
+
+    return { success: true, error: false };
+  } catch (error) {
+    console.log(error);
+    return { success: false, error: true };
+  }
+};
+
+export const updateSubject = async (
+  currentState: CurrentState,
+  data: SubjectSchema
+) => {
+  try {
+    await prisma.subject.update({
+      where: {
+        id: data.id,
+      },
+      data: {
+        name: data.name,
+        teachers: {
+          set: data.teachers.map((teacherId) => ({ id: teacherId })),
+        },
+      },
+    });
+
+    // revalidatePath("/list/subjects");
+
+    return { success: true, error: false };
+  } catch (error) {
+    console.log(error);
+    return { success: false, error: true };
+  }
+};
+
+export const deleteSubject = async (
+  currentState: CurrentState,
+  data: FormData
+) => {
+  const id = data.get("id") as string;
+  try {
+    await prisma.subject.delete({
+      where: {
+        id: parseInt(id),
+      },
+    });
+
+    // revalidatePath("/list/subjects");
+
+    return { success: true, error: false };
+  } catch (error) {
+    console.log(error);
+    return { success: false, error: true };
+  }
+};
+
+// Class
+export const createClass = async (
+  currentState: CurrentState,
+  data: ClassSchema
+) => {
+  try {
+    await prisma.class.create({
+      data,
+    });
+
+    // revalidatePath("/list/class");
+
+    return { success: true, error: false };
+  } catch (error) {
+    console.log(error);
+    return { success: false, error: true };
+  }
+};
+
+export const updateClass = async (
+  currentState: CurrentState,
+  data: ClassSchema
+) => {
+  try {
+    await prisma.class.update({
+      where: {
+        id: data.id,
+      },
+      data,
+    });
+
+    // revalidatePath("/list/class");
+
+    return { success: true, error: false };
+  } catch (error) {
+    console.log(error);
+    return { success: false, error: true };
+  }
+};
+
+export const deleteClass = async (
+  currentState: CurrentState,
+  data: FormData
+) => {
+  const id = data.get("id") as string;
+  try {
+    await prisma.class.delete({
+      where: {
+        id: parseInt(id),
+      },
+    });
+
+    // revalidatePath("/list/class");
+
+    return { success: true, error: false };
+  } catch (error) {
+    console.log(error);
+    return { success: false, error: true };
+  }
+};
+
+// Teacher
+export const createTeacher = async (
+  currentState: CurrentState,
+  data: TeacherSchema
+) => {
+  try {
+    const user = await clerkClient.users.createUser({
+      username: data.username,
+      password: data.password,
+      firstName: data.name,
+      lastName: data.surname,
+      publicMetadata: {role: "teacher"},
+    })
+    await prisma.teacher.create({
+      data: {
+        id: user.id,
+        username: data.username,
+        name: data.name,
+        surname: data.surname,
+        email: data.email,
+        phone: data.phone,
+        address: data.address,
+        img: data.img,
+        bloodType: data.bloodType,
+        sex: data.sex,
+        birthday: data.birthday,
+        subjects: {
+          connect: data.subjects?.map((subjectId:string) => ({
+            id: parseInt(subjectId)
+          }))
+        },
+      },
+    });
+
+    // revalidatePath("/list/teachers");
+
+    return { success: true, error: false };
+  } catch (error) {
+    console.log(error);
+    return { success: false, error: true };
+  }
+};
+
+export const updateTeacher = async (
+  currentState: CurrentState,
+  data: TeacherSchema
+) => {
+  try {
+    await prisma.teacher.update({
+      where: {
+        id: data.id,
+      },
+      data: {},
+    });
+
+    // revalidatePath("/list/teachers");
+
+    return { success: true, error: false };
+  } catch (error) {
+    console.log(error);
+    return { success: false, error: true };
+  }
+};
+
+export const deleteTeacher = async (
+  currentState: CurrentState,
+  data: FormData
+) => {
+  const id = data.get("id") as string;
+  try {
+    await prisma.teacher.delete({
+      where: {
+        id,
+      },
+    });
+
+    // revalidatePath("/list/teachers");
+
+    return { success: true, error: false };
+  } catch (error) {
+    console.log(error);
+    return { success: false, error: true };
+  }
+};
